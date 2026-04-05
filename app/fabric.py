86300727cb9deb084b590a74bfd63db360a90d66
Ernesto <sereernest@gmail.com>
2026-04-06 02:47:59 +0300

    Update fabric.py

--- app/fabric.py
+++ app/fabric.py
@@ -1,1 +1,20 @@
 """Application Fabric"""
+
+from flask import Flask, render_template
+from flask_cors import CORS
+
+from .errors import add_error_handlers
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
+    @app.route('/', methods=["GET"])
+    def read_root():
+        return render_template('index.html')
+    add_error_handlers(app)
+    return app
+
+if __name__ == '__main__':
+    application: Flask = create_app()
