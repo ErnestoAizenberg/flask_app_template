67928cc9300164d5754bb26d67a19c81cc966a34
Ernesto <sereernest@gmail.com>
2026-04-09 01:37:49 +0300

    feat: extend config

--- app/fabric.py
+++ app/fabric.py
@@ -2,6 +2,8 @@
 
 __all__ == ['create_app']
 
+from typing import Optional
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -13,8 +15,11 @@ def register_handlers(app: Flask) -> None:
     app.add_url_rule('/', 'read_root', views.read_root)
 
 
-def create_app() -> Flask:
-    app = Flask(__name__)
+def create_app(config_path: Optional[str] = None, name: str = __name__) -> Flask:
+    app = Flask(name)
+    if config_path:
+        app.config.from_object(config_path)
+    
     CORS(app, resources={r'/*': {'origins': '*'}})
     register_handlers(app) 
     return app
